6033c555dc863744ee484441e7dd238d439e5aae
Matt Costuros <matt.costuros@gmail.com>
2024-11-20 08:44:23 -0500

    Initial Migrations

---
+++ src/MacroTracker/Migrations/20241120132938_InitialCreate.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MacroTracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Entries",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ProteinGrams = table.Column<int>(type: "int", nullable: false),
+                    FatGrams = table.Column<int>(type: "int", nullable: false),
+                    CarbsGrams = table.Column<int>(type: "int", nullable: false),
+                    FiberGrams = table.Column<int>(type: "int", nullable: false),
+                    OwnerId = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Entries", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Favorites",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProteinGrams = table.Column<int>(type: "int", nullable: false),
+                    FatGrams = table.Column<int>(type: "int", nullable: false),
+                    CarbsGrams = table.Column<int>(type: "int", nullable: false),
+                    FiberGrams = table.Column<int>(type: "int", nullable: false),
+                    OwnerId = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Favorites", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Entries");
+
+            migrationBuilder.DropTable(
+                name: "Favorites");
+        }
+    }
+}
